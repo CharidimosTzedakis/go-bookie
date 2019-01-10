25ba86feec7ee290ad3bbfaccd81828c32322d88
Charidimos Tzedakis <harris.jedakis@gmail.com>
2019-01-11 00:33:47 +0200

    first render of App Login widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {AppLogin} from './components'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Please login to go-bookie:
+          <AppLogin/>
+          <button>Continue with facebook</button>
         </header>
       </div>
     );
---
+++ src/components/AppLogin/AppLogin.styles.js
@@ -0,0 +1,8 @@
+import { styled } from 'styled-components';
+import AppLoginComponent from './AppLogin.component'
+
+const AppLoginWidget = styled.AppLoginComponent`
+    border: 1px solid green;
+`;
+
+export { AppLoginWidget };
