45dc0989713b553f896983fc6251f84c80c1921c
Charidimos Tzedakis <harris.jedakis@gmail.com>
2019-01-13 11:22:44 +0200

    login screen first version with 3 buttons

--- src/components/AppLogin/AppLogin.component.tsx
+++ src/components/AppLogin/AppLogin.component.tsx
@@ -10,9 +10,17 @@ const AppLoginComponent: SFC<Props> = (props) => {
         <div className={props.className}>
             <span>Username:<input/></span>
             <span id="password">Password:<input type="password"/></span>
-            <div className="fb-login-button" data-max-rows="1" data-size="medium" data-button-type="continue_with" 
-                data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false">
-            </div>
+        <div className="loginButtons">
+            <button className="loginBtn bookieLogin">
+                Login
+            </button>
+            <button className="loginBtn loginBtn--facebook">
+                Login with Facebook
+            </button>
+            <button className="loginBtn loginBtn--google">
+                Login with Google
+            </button>
+        </div>
         </div>
     );
 }
@@ -21,6 +29,7 @@ const AppLoginComponent: SFC<Props> = (props) => {
 const AppLoginWidget = styled(AppLoginComponent)/*css*/`
     @import url('https://fonts.googleapis.com/css?family=Montserrat');
     margin-top: 20px;
+    width: 350px;
     span {
         display: block;
         font-size: 16px;
@@ -46,14 +55,78 @@ const AppLoginWidget = styled(AppLoginComponent)/*css*/`
     span#password {
         padding-left: 7px;
     }
-    button {
-        margin-top: 40px;
-    }
 
-    .fb-login-button {
-        margin-top: 20px;
-        
-    }
+    .loginBtn {
+        display: block;
+        float: right;
+        width: 207px;
+        box-sizing: border-box;
+        position: relative;
+        /* width: 13em;  - apply for fixed size */
+        margin: 0.2em;
+        padding: 0 15px 0 46px;
+        border: none;
+        text-align: left;
+        line-height: 34px;
+        white-space: nowrap;
+        border-radius: 0.2em;
+        font-size: 16px;
+        color: #FFF;
+      }
+      .loginBtn:before {
+        content: "";
+        box-sizing: border-box;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 34px;
+        height: 100%;
+      }
+      .loginBtn:focus {
+        outline: none;
+      }
+      .loginBtn:active {
+        box-shadow: inset 0 0 0 32px rgba(0,0,0,0.1);
+      }
+      
+      /* Facebook */
+      .loginBtn--facebook {
+        background-color: #4C69BA;
+        background-image: linear-gradient(#4C69BA, #3B55A0);
+        /*font-family: "Helvetica neue", Helvetica Neue, Helvetica, Arial, sans-serif;*/
+        text-shadow: 0 -1px 0 #354C8C;
+      }
+      .loginBtn--facebook:before {
+        border-right: #364e92 1px solid;
+        background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png') 6px 6px no-repeat;
+      }
+      .loginBtn--facebook:hover,
+      .loginBtn--facebook:focus {
+        background-color: #5B7BD5;
+        background-image: linear-gradient(#5B7BD5, #4864B1);
+      }
+      
+      /* Google */
+      .loginBtn--google {
+        /*font-family: "Roboto", Roboto, arial, sans-serif;*/
+        background: #DD4B39;
+      }
+      .loginBtn--google:before {
+        border-right: #BB3F30 1px solid;
+        background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_google.png') 6px 6px no-repeat;
+      }
+      .loginBtn--google:hover,
+      .loginBtn--google:focus {
+        background: #E74B37;
+      }
+      .bookieLogin {
+        background-color: #F91659;
+      }
+
+      .loginButtons {
+          margin-top: 30px;
+          margin-right: 5px;
+      }
 `;
 
 export default AppLoginWidget;
